2dcfc16f30305f1f1cf20093bea45fc0dc1bb820
ysdy44 <ysdy44@outlook.com>
2021-05-02 21:30:12 +0800

    New ColorPicker

--- HsvColorPickers/ColorPicker.xaml.cs
+++ HsvColorPickers/ColorPicker.xaml.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 
 namespace HSVColorPickers
@@ -17,60 +18,26 @@ namespace HSVColorPickers
         //@Delegate
         /// <summary> Occurs when the color value changed. </summary>
         public event ColorChangeHandler ColorChanged;
-        /// <summary> Occurs when the color change starts. </summary>
-        public event ColorChangeHandler ColorChangeStarted;
-        /// <summary> Occurs when color change. </summary>
-        public event ColorChangeHandler ColorChangeDelta;
-        /// <summary> Occurs when the color change is complete. </summary>
-        public event ColorChangeHandler ColorChangeCompleted;
+        /// <summary> Occurs when the color changed starts. </summary>
+        public event ColorChangeHandler ColorChangedStarted;
+        /// <summary> Occurs when color changed. </summary>
+        public event ColorChangeHandler ColorChangedDelta;
+        /// <summary> Occurs when the color changed is complete. </summary>
+        public event ColorChangeHandler ColorChangedCompleted;
 
 
-        //@Group
-        private EventHandler<int> Group;
         private EventHandler<Color> ChangeColor;
+
 
         /// <summary> Gets picker's type name. </summary>
         public string Type => "Color";
         /// <summary> Gets picker self. </summary>
         public Control Self => this;
         /// <summary> Gets hex picker. </summary>
-        public TextBox HexPicker => this._HexPicker;
-
-
-        private IEnumerable<IColorPicker> ColorPickers()
-        {
-            yield return this.SwatchesPicker;
-            yield return this.WheelPicker;
-            yield return this.RGBPicker;
-            yield return this.HSVPicker;
-
-            yield return this.PaletteHuePicker;
-            yield return this.PaletteSaturationPicker;
-            yield return this.PaletteValuePicker;
-
-            yield return this.CirclePicker;
-        }
+        public TextBox HexPicker => this.HexPickerCore;
 
 
         #region DependencyProperty
-
-
-        /// <summary> Get or set index of the current picker. </summary>
-        public int Index
-        {
-            get { return (int)GetValue(IndexProperty); }
-            set { SetValue(IndexProperty, value); }
-        }
-        /// <summary> Identifies the <see cref = "ColorPicker.Index" /> dependency property. </summary>
-        public static readonly DependencyProperty IndexProperty = DependencyProperty.Register(nameof(Index), typeof(int), typeof(ColorPicker), new PropertyMetadata(0, (sender, e) =>
-        {
-            ColorPicker con = (ColorPicker)sender;
-
-            if (e.NewValue is int value)
-            {
-                con.Group?.Invoke(con, value);
-            }
-        }));
 
 
         /// <summary> Gets or sets picker's color. </summary>
@@ -88,7 +55,7 @@ namespace HSVColorPickers
 
                 Color color = Color.FromArgb(255, value.R, value.G, value.B);
                 this.ChangeColor?.Invoke(this, color);//Delegate
-                this._HexPicker.Color = color;
+                this.HexPickerCore.Color = color;
 
                 this.R = value.R;
                 this.G = value.G;
@@ -96,63 +63,51 @@ namespace HSVColorPickers
             }
         }
 
-        private Color _Color
+        private void OnColorChanged(object sender, Color value)
         {
-            set
-            {
-                if (value.A == this.Alpha) if (value.R == this.R) if (value.G == this.G) if (value.B == this.B) return;
+            if (value.A == this.Alpha) if (value.R == this.R) if (value.G == this.G) if (value.B == this.B) return;
 
-                this.R = value.R;
-                this.G = value.G;
-                this.B = value.B;
+            this.R = value.R;
+            this.G = value.G;
+            this.B = value.B;
 
-                Color color = Color.FromArgb(this.Alpha, value.R, value.G, value.B);
-                this.ColorChanged?.Invoke(this, color);//Delegate
-                this._HexPicker.Color = color;
-            }
+            Color color = Color.FromArgb(this.Alpha, value.R, value.G, value.B);
+            this.ColorChanged?.Invoke(this, color);//Delegate
+            this.HexPickerCore.Color = color;
         }
-        private Color _ColorStarted
+        private void OnColorChangedStarted(object sender, Color value)
         {
-            set
-            {
-                //if (value.A == this.Alpha) if (value.R == this.R) if (value.G == this.G) if (value.B == this.B) return;
+            //if (value.A == this.Alpha) if (value.R == this.R) if (value.G == this.G) if (value.B == this.B) return;
 
-                this.R = value.R;
-                this.G = value.G;
-                this.B = value.B;
+            this.R = value.R;
+            this.G = value.G;
+            this.B = value.B;
 
-                Color color = Color.FromArgb(this.Alpha, value.R, value.G, value.B);
-                this.ColorChangeStarted?.Invoke(this, color);//Delegate
-            }
+            Color color = Color.FromArgb(this.Alpha, value.R, value.G, value.B);
+            this.ColorChangedStarted?.Invoke(this, color);//Delegate
         }
-        private Color _ColorDelta
+        private void OnColorChangedDelta(object sender, Color value)
         {
-            set
-            {
-                if (value.A == this.Alpha) if (value.R == this.R) if (value.G == this.G) if (value.B == this.B) return;
+            //if (value.A == this.Alpha) if (value.R == this.R) if (value.G == this.G) if (value.B == this.B) return;
 
-                this.R = value.R;
-                this.G = value.G;
-                this.B = value.B;
+            this.R = value.R;
+            this.G = value.G;
+            this.B = value.B;
 
-                Color color = Color.FromArgb(this.Alpha, value.R, value.G, value.B);
-                this.ColorChangeDelta?.Invoke(this, color);//Delegate
-            }
+            Color color = Color.FromArgb(this.Alpha, value.R, value.G, value.B);
+            this.ColorChangedDelta?.Invoke(this, color);//Delegate
         }
-        private Color _ColorCompleted
+        private void OnColorChangedCompleted(object sender, Color value)
         {
-            set
-            {
-                //if (value.A == this.Alpha) if (value.R == this.R) if (value.G == this.G) if (value.B == this.B) return;
+            //if (value.A == this.Alpha) if (value.R == this.R) if (value.G == this.G) if (value.B == this.B) return;
 
-                this.R = value.R;
-                this.G = value.G;
-                this.B = value.B;
+            this.R = value.R;
+            this.G = value.G;
+            this.B = value.B;
 
-                Color color = Color.FromArgb(this.Alpha, value.R, value.G, value.B);
-                this.ColorChangeCompleted?.Invoke(this, color);//Delegate
-                this._HexPicker.Color = color;
-            }
+            Color color = Color.FromArgb(this.Alpha, value.R, value.G, value.B);
+            this.ColorChangedCompleted?.Invoke(this, color);//Delegate
+            this.HexPickerCore.Color = color;
         }
 
 
@@ -164,22 +119,11 @@ namespace HSVColorPickers
             set => this.AlphaPicker.Alpha = value;
         }
 
-        private byte _Alpha
-        {
-            set => this.ColorChanged?.Invoke(this, Color.FromArgb(value, this.R, this.G, this.B)); //Delegate
-        }
-        private byte _AlphaStarted
-        {
-            set => this.ColorChangeStarted?.Invoke(this, Color.FromArgb(value, this.R, this.G, this.B)); //Delegate
-        }
-        private byte _AlphaDelta
-        {
-            set => this.ColorChangeDelta?.Invoke(this, Color.FromArgb(value, this.R, this.G, this.B)); //Delegate
-        }
-        private byte _AlphaCompleted
-        {
-            set => this.ColorChangeCompleted?.Invoke(this, Color.FromArgb(value, this.R, this.G, this.B)); //Delegate
-        }
+        private void OnAlphaChanged(object sender, byte value) => this.ColorChanged?.Invoke(this, Color.FromArgb(value, this.R, this.G, this.B)); //Delegate  
+        private void OnAlphaChangedStarted(object sender, byte value) => this.ColorChangedStarted?.Invoke(this, Color.FromArgb(value, this.R, this.G, this.B)); //Delegate
+        private void OnAlphaChangedDelta(object sender, byte value) => this.ColorChangedDelta?.Invoke(this, Color.FromArgb(value, this.R, this.G, this.B)); //Delegate
+        private void OnAlphaChangedCompleted(object sender, byte value) => this.ColorChangedCompleted?.Invoke(this, Color.FromArgb(value, this.R, this.G, this.B)); //Delegate
+
 
         /// <summary> Gets or sets picker's red. </summary>
         private byte R = 255;
@@ -248,6 +192,10 @@ namespace HSVColorPickers
         #endregion
 
 
+        private readonly IEnumerable<IColorPicker> ColorPickers;
+        private readonly IEnumerable<GridViewItem> Headers;
+
+
         //@Construct
         /// <summary>
         /// Construct a ColorPicker.
@@ -255,90 +203,93 @@ namespace HSVColorPickers
         public ColorPicker()
         {
             this.InitializeComponent();
-
-            //Picker
-            IList<IColorPicker> colorPickers = this.ColorPickers().ToList();
-            this.Loaded += (s, e) => this.ConstructColorPickers(colorPickers);
-            this.ComboBox.ItemsSource = from colorPicker in colorPickers select colorPicker.Type;
+            this.ColorPickers = from child in this.BodyGrid.Children where child is IColorPicker select child as IColorPicker;
+            this.Headers = from child in this.SegmentedGrid.Children where child is GridViewItem select child as GridViewItem;
+            this.ConstructColorPickers();
+            this.Loaded += this.ColorPicker_Loaded;
 
             //Alpha
             this.Alpha = 255;
-            this.AlphaPicker.AlphaChanged += (s, value) => this._Alpha = value;
-            this.AlphaPicker.AlphaChangeStarted += (s, value) => this._Alpha = value;
-            this.AlphaPicker.AlphaChangeDelta += (s, value) => this._Alpha = value;
-            this.AlphaPicker.AlphaChangeCompleted += (s, value) => this._Alpha = value;
+            this.AlphaPicker.AlphaChanged += this.OnAlphaChanged;
+            this.AlphaPicker.AlphaChangeStarted += this.OnAlphaChangedStarted;
+            this.AlphaPicker.AlphaChangeDelta += this.OnAlphaChangedDelta;
+            this.AlphaPicker.AlphaChangeCompleted += this.OnAlphaChangedCompleted;
 
             //Hex
-            this._HexPicker.Color = this.Color;
-            this._HexPicker.ColorChanged += (s, color) =>
+            this.HexPickerCore.Color = this.Color;
+            this.HexPickerCore.ColorChanged += (s, color) =>
             {
-                this._Color = color;
+                this.OnColorChanged(this, color);
                 this.ChangeColor?.Invoke(this, color);
             };
-            //Straw
-            this._StrawPicker.Color = this.Color;
-            this._StrawPicker.ColorChanged += (s, color) =>
+
+            //Eyedropper
+            this.Eyedropper.Color = this.Color;
+            this.Eyedropper.ColorChanged += (s, color) =>
             {
-                this._Color = color;
+                this.OnColorChanged(this, color);
                 this.ChangeColor?.Invoke(this, color);
             };
         }
 
-    }
 
-    /// <summary>
-    /// Color picker (ง •̀_•́)ง
-    /// </summary>
-    public sealed partial class ColorPicker : UserControl
-    {
         //IColorPicker
-        private void ConstructColorPickers(IList<IColorPicker> colorPickers)
+        private void ConstructColorPickers()
         {
-            int index = 0;
-
-            //Pickers
-            foreach (IColorPicker colorPicker in colorPickers)
+            foreach (IColorPicker colorPicker2 in this.ColorPickers)
             {
-                this.ConstructGroup(colorPicker, index);
-                index++;
+                this.ChangeColor += (s, color) =>
+                {
+                    if (colorPicker2.Self.Visibility == Visibility.Visible)
+                    {
+                        colorPicker2.Color = color;
+                    }
+                };
+            }
+
+            foreach (GridViewItem header in this.Headers)
+            {
+                header.Tapped += (s, e) => this.ItemClick(header);
             }
         }
 
-        //Group
-        private void ConstructGroup(IColorPicker colorPicker, int index)
+        private void ColorPicker_Loaded(object sender, RoutedEventArgs e)
         {
-            void group(int groupIndex)
-            {
-                if (groupIndex == index)
-                {
-                    colorPicker.Self.Visibility = Visibility.Visible;
+            this.Loaded -= this.ColorPicker_Loaded;
+            this.ItemClick(this.Circle);
+        }
 
+        private void ItemClick(GridViewItem header)
+        {
+            string type = header.Name;
+
+            foreach (GridViewItem item2 in this.Headers)
+            {
+                item2.IsSelected = item2.Name == type;
+            }
+
+            foreach (IColorPicker colorPicker in this.ColorPickers)
+            {
+                if (colorPicker.Type == type)
+                {
+                    colorPicker.ColorChanged += this.OnColorChanged;
+                    colorPicker.ColorChangedStarted += this.OnColorChangedStarted;
+                    colorPicker.ColorChangedDelta += this.OnColorChangedDelta;
+                    colorPicker.ColorChangedCompleted += this.OnColorChangedCompleted;
+
+                    colorPicker.Self.Visibility = Visibility.Visible;
                     colorPicker.Color = this.Color;
                 }
                 else
                 {
+                    colorPicker.ColorChanged -= this.OnColorChanged;
+                    colorPicker.ColorChangedStarted -= this.OnColorChangedStarted;
+                    colorPicker.ColorChangedDelta -= this.OnColorChangedDelta;
+                    colorPicker.ColorChangedCompleted -= this.OnColorChangedCompleted;
+
                     colorPicker.Self.Visibility = Visibility.Collapsed;
                 }
             }
-
-            //NoneButton
-            group(this.Index);
-
-            //Buttons
-            colorPicker.ColorChanged += (s, value) => this._Color = value;
-            colorPicker.ColorChangeStarted += (s, value) => this._ColorStarted = value;
-            colorPicker.ColorChangeDelta += (s, value) => this._ColorDelta = value;
-            colorPicker.ColorChangeCompleted += (s, value) => this._ColorCompleted = value;
-
-            //Change
-            this.Group += (s, e) => group(e);
-            this.ChangeColor += (s, color) =>
-            {
-                if (this.Index == index)
-                {
-                    colorPicker.Color = color;
-                }
-            };
         }
 
     }
